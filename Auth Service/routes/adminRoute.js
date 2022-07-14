2d65c2cf3497133fc09ee8446367abe0808b66b9
Victor Muriithi <89382592+Victor-Muriithi@users.noreply.github.com>
2022-07-14 09:59:43 +0300

    Delete adminRoute.js

--- Auth Service/routes/adminRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const adminRouter = express.Router();
-
-const {addProducts, deleteProduct} = require('../controllers/adminControllers')
-
-adminRouter.get('/addProducts', addProducts)
-adminRouter.get('/deleteproduct', deleteProduct)
-
-module.exports = {adminRouter}
