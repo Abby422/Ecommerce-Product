a3d0a43e0f0548536b478980fe70cbf04a9ebabb
Victor-Muriithi <vmurithi10@gmail.com>
2022-07-15 17:14:16 +0300

    rabbit mq

--- Auth Service/controllers/userController.js
+++ Auth Service/controllers/userController.js
@@ -21,7 +21,34 @@ const userControllers = {
                     user: { userName, email },
                     message: 'user added',
                     token:token,
-                }); return;
+                });
+
+                try {
+                    async function connect() {
+                      const amqpServer = "amqp://localhost:15672";
+                      connection = await amqp.connect(amqpServer);
+                      channel = await connection.createChannel();
+                      await channel.assertQueue("registrationDetails");
+                    }
+                    connect();
+                    
+                    channel.sendToQueue(
+                      "registrationDetails",
+                      Buffer.from(
+                          JSON.stringify({
+                              name,
+                              email,
+                          })
+                      )
+                  )
+                   channel.assertQueue("registrationDetails");      
+                  await channel.consume("registrationDetails", (data)=>{
+                    console.log(JSON.parse(data.content))
+                    channel.ack(data)
+                  })
+                  } catch (error) {
+                    console.log(error.message)
+                  }
             }
 
 
