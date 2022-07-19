be1302e350c12e05b65b349d192a713299b0db7f
Victor-Muriithi <vmurithi10@gmail.com>
2022-07-19 16:56:12 +0300

    adminMiddleware

--- Backend/Business Logic/Routes/userRoutes.js
+++ Backend/Business Logic/Routes/userRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const { search, order} = require('../controllers/userController')
+const adminAuth = require("../../Middlewares/adminMiddleware");
+const { search, order,setAdmin } = require('../Controllers/userController');
 
 router.post('/search', search)
 router.post('/checkout', order)
+router.post('/setAdmin', adminAuth('testuser1@youtu.be'), setAdmin )
 
 
 module.exports = { router };
