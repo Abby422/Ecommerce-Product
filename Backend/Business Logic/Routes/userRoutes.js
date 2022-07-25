d19f69a3f41d721b7f556825b9fdd4aa6a56d5de
Victor-Muriithi <vmurithi10@gmail.com>
2022-07-25 16:00:38 +0300

    pagination

--- Backend/Business Logic/Routes/userRoutes.js
+++ Backend/Business Logic/Routes/userRoutes.js
@@ -6,7 +6,7 @@ const { search, order,setAdmin, getAllProducts, getProductDetails } = require('.
 router.post('/search', search)
 router.post('/checkout', order)
 // router.post('/setAdmin', adminAuth, setAdmin)
-router.get('/allProducts', getAllProducts)
+router.post('/allProducts', getAllProducts)
 router.get('/product/:id', getProductDetails)
 
 module.exports = { router }
