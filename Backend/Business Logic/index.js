1203e6f4e42cea23e9f412eb762f40376b7d647f
Victor-Muriithi <vmurithi10@gmail.com>
2022-07-26 12:47:11 +0300

    hashed passwords && cors

--- Backend/Business Logic/index.js
+++ Backend/Business Logic/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 require("dotenv").config();
-// const cors = require('cors')
-const { router } = require("./Routes/userRoutes");
+const cors = require('cors')
+const { router } = require("./Routes/productsRoutes");
 const app = express();
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json());
 app.use("/", router);
 const port = process.env.PORT || 5001;
